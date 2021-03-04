62339fed41f83f773961e7e7cf837fa64d0edb06
shrotriprasad <33006209+shrotriprasad@users.noreply.github.com>
2021-03-04 12:01:00 -0600

    com

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 'use strict';
 // Module Dependencies
 // -------------------
-var express     = require('express');
-var bodyParser  = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 var errorhandler = require('errorhandler');
-var http        = require('http');
-var path        = require('path');
-var request     = require('request');
-var routes      = require('./routes');
-var activity    = require('./routes/activity');
+var http = require('http');
+var path = require('path');
+var request = require('request');
+var routes = require('./routes');
+var activity = require('./routes/activity');
 
 var app = express();
 
 // Configure Express
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json({type: 'application/json'})); 
+app.use(bodyParser.json({ type: 'application/json' }));
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.methodOverride());
@@ -24,20 +24,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
-  app.use(errorhandler());
+    app.use(errorhandler());
 }
 
 // HubExchange Routes
-app.get('/', routes.index );
-app.post('/login', routes.login );
-app.post('/logout', routes.logout );
+app.get('/', routes.index);
+app.post('/login', routes.login);
+app.post('/logout', routes.logout);
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+app.post('/save/', activity.save);
+app.post('/validate/', activity.validate);
+app.post('/publish/', activity.publish);
+app.post('/execute/', activity.execute);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
